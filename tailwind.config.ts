f3f3650a51f2b27a9ea9ca1b210aa12aa6f0a893
Jhyden Ellana <jhydenellana@gmail.com>
2024-08-09 16:04:43 +0800

    💄 Fix style colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,8 @@ export default {
         primary: "hsl(var(--primary) / <alpha-value>)",
         secondary: "hsl(var(--secondary) / <alpha-value>)",
         accent: "hsl(var(--accent) / <alpha-value>)",
-        negative: "var(--negative) / <alpha-value>)",
-        positive: "var(--positive) / <alpha-value>)",
+        negative: "hsl(var(--negative) / <alpha-value>)",
+        positive: "hsl(var(--positive) / <alpha-value>)",
       },
       animation,
       // @ts-expect-error
