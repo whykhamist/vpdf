d0293ab41e45fa484813ddffeea5d82bae5d81a7
Jhyden Ellana <jhydenellana@gmail.com>
2025-03-05 18:02:36 +0800

    🔨ops: export composables - usePdf - usePdfViewer

--- packages/vpdf/src/components/index.ts
+++ packages/vpdf/src/components/index.ts
@@ -1,4 +1,7 @@
 import { defineAsyncComponent } from "vue";
+import { usePdf, usePdfViewer } from "../composables";
+
+export { usePdf, usePdfViewer };
 
 export default {
   VPdf: defineAsyncComponent(() => import("./vPdf.vue")),
