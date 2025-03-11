dc6a11bed14777b3e22f1bff7a33be7e5dc9ed2f
Jhyden Ellana <jhydenellana@gmail.com>
2025-03-11 10:45:16 +0800

    feat: ✨ Add Icon component for preview page - Github svg icon - Npm svg icon

---
+++ packages/preview/src/components/icon/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed, defineAsyncComponent } from "vue";
+
+const props = defineProps<{
+  name: string;
+}>();
+
+const icon = defineAsyncComponent(() => import(`./svg/${props.name}.vue`));
+</script>
+
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
